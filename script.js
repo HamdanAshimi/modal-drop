b80cb8e316db1f9ab3073ea7d1135f3454925f04
HamdanAshimi <ashimiademola4@gmail.com>
2025-11-28 01:31:50 +0100

    Updated javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-'use strict';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
